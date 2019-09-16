9a857afd7e9318ae1a5459266aaf4cc5abf4da49
Shin <jmshin@a483e74fa0f8.ant.amazon.com>
2019-09-16 16:21:54 +0200

    tmux latest for fedora

--- .install.sh
+++ .install.sh
@@ -35,7 +35,17 @@ rbenv install $RUBY_VERSION
 rbenv global $RUBY_VERSION
 
 ################# Tmux #################
-sudo apt-get install tmux
+wget https://github.com/downloads/libevent/libevent/libevent-2.0.21-stable.tar.gz
+tar xzvf libevent-2.0.21-stable.tar.gz
+cd libevent-2.0.21-stable
+./configure && make
+sudo make install
+
+wget https://github.com/tmux/tmux/releases/download/2.4/tmux-2.4.tar.gz
+tar xzf tmux-2.4.tar.gz
+cd tmux-2.4
+./configure && make
+sudo make install
 
 # tmuxinator
 gem install --user-install tmuxinator
