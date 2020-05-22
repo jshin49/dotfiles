8e479c5f128293c9c35974e70baab7a2231b35a6
Jay S <jshin49@gmail.com>
2020-05-22 13:33:23 +0900

    Update .install.sh

--- .install.sh
+++ .install.sh
@@ -20,6 +20,9 @@ vim +PlugInstall +qall
 # neovim
 # sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 # sudo yum install -y neovim python3-neovim
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt-get update
+sudo apt-get install neovim
 
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
