45d821f087ec8161ea2088b45ae6c66236c2e161
Jay S <jshin49@gmail.com>
2023-06-09 00:23:50 +0900

    Update setup_gcp.sh

--- setup_gcp.sh
+++ setup_gcp.sh
@@ -3,6 +3,12 @@ ssh-keygen -t ed25519 -C "jshin49@gmail.com"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519
 
+# python build dep
+sudo apt update; sudo apt install build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev curl \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+
+
 # set up vim
 # vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
